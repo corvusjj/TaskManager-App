3e826b03a55c6dba0a4e75dd437b7696af8893e0
John <mjohncorbet@gmail.com>
2023-05-18 21:33:10 +0800

    setup fileId module

--- src/app.js
+++ src/app.js
@@ -1,1 +1,39 @@
-console.log('hello');
+const files = (() => {
+    let projects = [];
+    let tasks = [];
+
+    return { projects, tasks }
+})();
+
+const fileId = (() => {
+    let usedIDs = [];
+
+    const generateId = () => {
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
+        let randomId = '';
+
+        function placeCharacters() {
+            for (let i = 0; i < 8; i++) {
+                const randomIndex = Math.floor(Math.random() * characters.length);
+                randomId += characters.charAt(randomIndex);
+            }
+        }
+
+        placeCharacters();
+
+        if(usedIDs.includes(randomId)) {
+            randomId = '';
+            placeCharacters();
+        };
+
+        usedIDs.push(randomId);
+        return randomId;
+    }
+
+    const removeId = (id) => {
+        const index = usedIDs.indexOf(id);
+        usedIDs.splice(index, 1);
+    }
+
+    return { generateId, removeId}
+})();
