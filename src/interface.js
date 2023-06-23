def616deaed10de0cd81c9c71ef3dc3ee6c13152
John <mjohncorbet@gmail.com>
2023-06-23 23:10:00 +0800

    implement add project listener

--- src/interface.js
+++ src/interface.js
@@ -29,6 +29,7 @@ const Interface = (() => {
         }
 
         const generateFavoritesToNav = () => {
+            favoriteList.innerHTML = '';
             const favorites = projectManager.projects.filter((project) => project.favorite);
             favorites.forEach((project) => addProjectToFavorites(project));
         }
@@ -56,6 +57,7 @@ const Interface = (() => {
         }
 
         const generateProjectsToNav  = () => {
+            projectList.innerHTML = '';
             const projects = projectManager.projects.filter((proj) => proj.name !== 'Inbox@XFvW$W7');
             projects.forEach((project) => addProject(project));
         }
@@ -65,7 +67,7 @@ const Interface = (() => {
 
     const ProjectFormModule = (() => {
 
-        //  open/close projectForm & colorList
+        //  open/close projectForm & colorList listener
         const projectModal = document.querySelector('.add-project-modal');
         const projectForm = document.querySelector('.add-project-form');
         const addProjectIcon = document.querySelector('#add-project-icon');
@@ -121,7 +123,7 @@ const Interface = (() => {
             openColorList();
         });
 
-        //  set current project color
+        //  set current project color listener
         const colorOptions = document.querySelectorAll('.color-option');
         const colorDiv = colorBtn.querySelector('#current-color');
         const colorName = colorBtn.querySelector('p');
@@ -134,23 +136,45 @@ const Interface = (() => {
             });
         });
 
-        //  add newProject
+        //  style/set submit buttons to invalid or valid from input listener
         const addProjectBtn = document.querySelector('#add-project');
         const saveProjectBtn = document.querySelector('#save-project');
 
+        const toggleBtnStyle = (btn) => {
+            if (!projectForm.checkValidity()) {
+                btn.style.opacity = '0.5';
+                btn.setAttribute('disabled', '');
+            } else {
+                btn.style.opacity = '1';
+                btn.removeAttribute('disabled');
+            }
+        }   
+
+        projectForm.addEventListener('input', () => {
+            toggleBtnStyle(addProjectBtn);
+            toggleBtnStyle(saveProjectBtn);
+        });
+
+        //  add project listener
         addProjectBtn.addEventListener('click', (e) => {
 
             e.preventDefault();
-            const name = document.querySelector('#form-project-name');
-
             if (!projectForm.checkValidity()) {
                 return;
             }
 
-            console.log(name.value);
+            //  create new project
+            const name = document.querySelector('#form-project-name').value;
+            const color = document.querySelector('#form-project-color').dataset.colorSelected;
+            const favorite = document.querySelector('#favorites-checkbox').checked;
+
+            projectManager.createProject(name, color, favorite);
+            NavModule.generateProjectsToNav();
+            if (favorite === true) NavModule.generateFavoritesToNav();
+            
+            //  reset form to default
             closeProjectModal();
             projectForm.reset();
-
             toggleBtnStyle(addProjectBtn);
         });
     })();
